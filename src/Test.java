762a9efeda016f8cc9ce7d366707b60d33eb67c0
DavidYangYong <yyzhpq@163.com>
2017-06-03 10:25:55 +0800

    add13

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 		System.out.println(1);
 		System.out.println(2);
 		System.out.println(3);
-		// System.out.println(4);
+		System.out.println(4);
 		// System.out.println(7);
 		// System.out.println(8);
 	}
